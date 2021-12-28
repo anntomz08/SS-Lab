81658a84e51fb900a0ff802b716d8791c49100d8
Anna Thomas <70516088+anntomz08@users.noreply.github.com>
2021-12-28 13:03:23 +0530

    Exp 4

--- fcfsdisksched.c
+++ fcfsdisksched.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main(){
 	int head=0,max,st=0,n;
-	printf("Enter the maximum value of requests");
-	scanf("%d",&max);
 	printf("Enter the number of requests");
 	scanf("%d",&n);
+	printf("Enter the maximum value of requests");
+	scanf("%d",&max);
 	printf("Enter the position of head");
 	scanf("%d",&head);
 	int req[n];
